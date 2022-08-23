968a3069dcdc88d6e440b365cdbb387890e1690b
jackf <jack.freeman1990@gmail.com>
2022-08-23 14:15:32 +0100

    CORS Access config to allow access to API

---
+++ src/main/java/com/sakila/database/demo/CorsConfig.java
@@ -0,0 +1,19 @@
+package com.sakila.database.demo;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CorsConfig implements WebMvcConfigurer {
+    //enable cors access origin when posting/getting with the API
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowCredentials(true)
+                .allowedMethods("*")
+                .maxAge(3600);
+    }
+}
